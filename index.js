850dc27be133e2158850f3e6e3af94a4cee49069
Huy-Ngo <duchuy29092000@gmail.com>
2020-08-31 19:51:37 +0700

    Add new command for hot memes

--- index.js
+++ index.js
@@ -4,28 +4,37 @@ const { Client, MessageAttachment } = require('discord.js');
 const config = require('./config');
 const client = new Client();
 
+const sendMeme = (res) => {
+    let str = '';
+    res.on('data', (chunk) => {
+        str += chunk;
+    });
+
+    res.on('end', () => {
+        const fullResponse = JSON.parse(str);
+        const attachment = new MessageAttachment(fullResponse.url);
+        msg.channel.send(attachment);
+    });
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content.toLowerCase() === 'send meme') {
+    const message = msg.content.toLowerCase();
+    if (message === 'send meme') {
         http.get({
             hostname: config.apiHost,
             port: config.apiPort,
             path: '/memes/new'
-        }, (res) => {
-            let str = '';
-            res.on('data', (chunk) => {
-                str += chunk;
-            });
-
-            res.on('end', () => {
-                const fullResponse = JSON.parse(str);
-                const attachment = new MessageAttachment(fullResponse.url);
-                msg.channel.send(attachment);
-            });
-        });
+        }, sendMeme);
+    } else if (message === 'send hot meme') {
+        http.get({
+            hostname: config.apiHost,
+            port: config.apiPort,
+            path: 'memes/hot'
+        }, sendMeme)
     }
 });
 
